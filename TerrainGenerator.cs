6f6c21db6224e4117f68fbf964cabf95a1a531e7
atomicKloc <84042497+atomicKloc@users.noreply.github.com>
2023-01-22 10:43:46 -0500

    Add files via upload

---
+++ TerrainGenerator.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainGenerator : MonoBehaviour
+{
+    Mesh mesh;
+
+    Vector3[] vertecies;
+    int[] triangles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        CreateShape();
+    }
+
+    void CreateShape(){
+        vertecies = new Vector3[]
+        {
+            new Vector3(0,0,0),
+            new Vector3(0,0,1),
+            new Vector3(1,0,0)
+        };
+        triangles = new int[]
+        {
+            0,1,2
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ wheels.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;  
+
+public class wheels : MonoBehaviour
+{
+    private HingeJoint hinge;
+    private JointMotor motor;
+    // Start is called before the first frame update
+    void Start()
+    {   
+        hinge = GetComponent<HingeJoint>();
+        motor = hinge.motor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Input.GetAxis("Vertical"));
+        motor.targetVelocity = 1000f * Input.GetAxis("Vertical");
+        if (Input.GetAxis("Horizontal") < 0f){      
+            motor.targetVelocity -= 1000f;
+        }else{
+           // motor.freeSpin = true;
+        }
+        if(Input.GetAxis("Horizontal") > 0f){
+            motor.targetVelocity += 1000f;
+        }
+        hinge.motor = motor;
+    }
+}
